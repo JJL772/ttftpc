3b5d279e3d7a74798784d0fd4964be76e3e0189f
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2026-07-29 18:07:15 -0700

    performance metrics and verbosity for the test client

--- tftpcmd.c
+++ tftpcmd.c
@@ -1,7 +1,19 @@
+//-----------------------------------------------------------------------------------------
+// Copyright (C) 2026, Jeremy Lorelli
+//-----------------------------------------------------------------------------------------
+// Purpose: TFTP test
+//-----------------------------------------------------------------------------------------
+// This file is part of 'ttftp'. It is subject to the license terms in the
+// LICENSE file found in the top-level directory of this distribution.
+// No part of 'ttftp', including this file, may be copied, modified, propagated,
+// or otherwise distributed except according to the terms contained in the LICENSE file.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//-----------------------------------------------------------------------------------------
 #include <getopt.h>
 #include <limits.h>
 #include <sys/stat.h>
-
+#include <time.h>
 #include "ttftp.h"
 
 static void
@@ -13,12 +25,12 @@ show_help()
 int main(int argc, char** argv)
 {
   int opt;
-  int put = 0, dl = 0;
+  int put = 0, dl = 0, verbose = 0;
 
   char file[PATH_MAX] = {};
   char addr[128] = {};
   char out[PATH_MAX] = {};
-  while ((opt = getopt(argc, argv, "a:p:d:ho:")) != -1) {
+  while ((opt = getopt(argc, argv, "va:p:d:ho:")) != -1) {
     switch (opt) {
     case 'a':
       strcpy(addr, optarg);
@@ -33,6 +45,9 @@ int main(int argc, char** argv)
     case 'd':
       dl = 1;
       strcpy(file, optarg);
+      break;
+    case 'v':
+      verbose = 1;
       break;
     case 'h':
     default:
@@ -64,6 +79,9 @@ int main(int argc, char** argv)
     return -1;
   }
   
+  if (verbose)
+    tc->log_level = TFTP_LOG_DEBUG;
+  
   struct stat st;
   if (put && stat(file, &st) < 0) {
     perror("stat");
@@ -75,12 +93,18 @@ int main(int argc, char** argv)
   read(fd, buf, st.st_size);
   close(fd);
   
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  ssize_t nbytes = -1;
+
   if (put) {
     int r;
     if ((r=tftpc_put(tc, file, buf, st.st_size)) < 0) {
       printf("Put failed: %s (%d)\n", strerror(-r), r);
       return -1;
     }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    nbytes = st.st_size;
   }
   
   if (dl) {
@@ -91,7 +115,9 @@ int main(int argc, char** argv)
       printf("Get failed: %s (%d)\n", strerror(-r), r);
       return -1;
     }
-    
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    nbytes = size;
+
     FILE* fp = stdout;
     if (*out) {
       fp = fopen(out, "wb");
@@ -105,6 +131,9 @@ int main(int argc, char** argv)
     fwrite(data, size, 1, fp);
     if (fp != stdout)
       fclose(fp);
+    
+    float elapsed = (end.tv_sec + end.tv_nsec / 1e9) - (start.tv_sec + start.tv_nsec / 1e9);
+    printf("Elapsed: %.2f seconds (%.2f MB/s)\n", elapsed, (nbytes / elapsed) / 1e6);
     return 0;
   }
 }
